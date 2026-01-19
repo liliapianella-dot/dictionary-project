f6406051189362723a412af3108a7e6a3756641b
pianellalilia-dot <pianella.lilia@gmail.com>
2026-01-19 08:54:48 +0100

    Created the header

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div>Hello World of words</div>;
+  return (
+    <div className="App">
+      <h1 className="text-uppercase">Dictionary</h1>
+      <h2>What word do you want to look up?</h2>
+      <form>
+        <input
+          type="search"
+          name="search-word"
+          className="inputText"
+          autoFocus
+        />
+        <input type="submit" value="Search" className="inputSearchButton" />
+      </form>
+    </div>
+  );
 }
