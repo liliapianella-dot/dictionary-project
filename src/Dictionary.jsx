22b444ab2a9d0d012fa0fdb6d5bc0ec84a103618
pianellalilia-dot <pianella.lilia@gmail.com>
2026-01-19 11:25:19 +0100

    Added SheCodes Dictionary API

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [wordkey, setWordkey] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data.phonetic);
+    console.log(response.data.word);
+    console.log(response.data.meanings[0].example);
+  }
+
   function handleSearch(event) {
     event.preventDefault();
-    alert(`${wordkey}`);
+    let apiKey = "90303e49ba1c9f82cb5tf7o1afec334c";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${wordkey}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function saveWordKey(event) {
     setWordkey(event.target.value);
-    console.log(event.target.value);
   }
 
   return (
